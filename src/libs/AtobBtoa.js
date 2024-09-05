42eb00f7cf33837c8a5211df6af5c4b9489c2c15
Sergey Linev <S.Linev@gsi.de>
2024-09-05 15:46:03 +0200

    Use simpler atob/btoa in node.js

--- src/libs/AtobBtoa.js
+++ src/libs/AtobBtoa.js
@@ -1,6 +1,13 @@
-import { globalObject } from "./globalObject.js";
+let atob, btoa;
 
-const atob = globalObject.atob.bind(globalObject);
-const btoa = globalObject.btoa.bind(globalObject);
+// @if MODULE_FORMAT!='cjs'
+atob = globalThis.atob;
+btoa = globalThis.btoa;
+// @endif
+
+// @if MODULE_FORMAT='cjs'
+atob = str => Buffer.from(str, 'base64').toString('latin1')
+btoa = str => Buffer.from(str, 'latin1').toString('base64');
+// @endif
 
 export { atob, btoa };
