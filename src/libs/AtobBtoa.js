ad9062d4fff7e5ea2adf270726ece405317c25c4
Sergey Linev <S.Linev@gsi.de>
2024-09-06 12:47:49 +0200

    Check node when assign atob/btoa

--- src/libs/AtobBtoa.js
+++ src/libs/AtobBtoa.js
@@ -1,13 +1,11 @@
 let atob, btoa;
 
-// @if MODULE_FORMAT!='cjs'
-atob = globalThis.atob;
-btoa = globalThis.btoa;
-// @endif
-
-// @if MODULE_FORMAT='cjs'
-atob = str => Buffer.from(str, 'base64').toString('latin1')
-btoa = str => Buffer.from(str, 'latin1').toString('base64');
-// @endif
+if ((typeof process === 'object') && (typeof process.versions === 'object') && process.versions.node && process.versions.v8) {
+   atob = str => Buffer.from(str, 'base64').toString('latin1');
+   btoa = str => Buffer.from(str, 'latin1').toString('base64');
+} else {
+   atob = globalThis.atob;
+   btoa = globalThis.btoa;
+}
 
 export { atob, btoa };
